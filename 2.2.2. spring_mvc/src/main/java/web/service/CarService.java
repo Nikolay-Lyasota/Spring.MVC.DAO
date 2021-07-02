f27537208ca46f83d40a021efe53d3cf26689b56
Nikolay-Lyasota <nikolaylyasota@gmail.com>
2021-07-02 14:33:02 +0300

    init commit

--- 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -2,8 +2,8 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.model.Car;
 import web.dao.CarDao;
+import web.model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
